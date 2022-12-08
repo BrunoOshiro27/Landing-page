4556d93978407aa51fbff4c0ca55da6699bb88be
Bruno Oshiro <brunooshiro27@gmail.com>
2022-12-09 08:35:27 +0900

    arrumando CSS

---
+++ script.js
@@ -0,0 +1,18 @@
+/*const ctrlProduct = document.getElementsByClassName(".control-products")[0]
+
+function createProduct(){
+    ctrlProduct.innerHTML += `<div class="info-product">
+    <div class="empty-box"></div>
+    <span>Nome do produto</span>
+    <span>De: R$23,99</span>
+    <span><strong>Por: R$19,99</strong></span>
+    <span>ou 2x de R$9,99</span>
+    <button>Comprar</button>
+</div>`
+}
+
+for(i=0; i<5; i++){
+    createProduct()
+}
+
+console(ctrlProduct())*/
